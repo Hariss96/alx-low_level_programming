3bd481aca7bccec209b63edd2ad500736eddc352
Hariss96 <harissou.oumarou@imsp-uac.org>
2022-06-30 16:43:19 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ *   main - Entry point
+ *
+ *   Return: 0 after printing function
+ */
 int main(void)
-
 {puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	return (0);
 }
