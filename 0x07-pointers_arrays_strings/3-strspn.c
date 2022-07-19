aee3c0bee1c20d736a89b482040c34722d2f1ebd
Hariss96 <harissou.oumarou@imsp-uac.org>
2022-07-18 17:18:17 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,24 +8,25 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-unsigned int x;
-int i;
+	unsigned int bytes = 0;
+	int index;
 
-x = 0;
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
 
-while (*s)
-{
-for (i = 0; accept[i]; i++)
-{
-	if (*s == accept[i])
-	{
-		x++;
-		break;
+			else if (accept[index + 1] == '\0')
+				return (bytes);
+		}
+
+		s++;
 	}
-	else if (accept[i + 1] == '\0')
-	{
-		return (x);
-	}
-}
-s++;
+
+	return (bytes);
 }
