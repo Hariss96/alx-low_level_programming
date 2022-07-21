4b77388b3e8bf58509fdbb4aa185a2d348e1a429
Hariss96 <harissou.oumarou@imsp-uac.org>
2022-07-20 17:12:06 -0700

    2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -2,18 +2,19 @@
 
 /**
  * _strlen_recursion - length of string
- * @s:string
- * Return:int
+ * @s: The string to be measured.
+ *
+ * Return: The length of the string.
  */
 int _strlen_recursion(char *s)
 {
-	int i = 0;
+	int len = 0;
 
 	if (*s)
 	{
-		i++;
-		i += _strlen_recursion(s + 1);
+		len++;
+		len += _strlen_recursion(s + 1);
 	}
 
-	return (i);
+	return (len);
 }
